a6371a9a157e73a564b5b50b32026b8cb90e7146
Leticia-Sousa-DS <leticiasousassilva@hotmail.com>
2025-07-15 17:10:33 -0300

    feat: add initial components Banner and Link

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Banner from "./components/Banner"
+import Link from "./components/Link"
+
+function App() {
+  return (
+  <>
+    <Banner src={"./Imagens/Banner Desktop.png"} alt={"Banner do Luz e Cena para Desktop, com imagem de pessoas no cinema e formas abstratas"} />
+    <Link href="/" target="_blank">Link</Link>
+  </>
+  )
+}
+
+export default App
