52214ae97cb6736c7db69757b69c838ede18c742
Leticia-Sousa-DS <leticiasousassilva@hotmail.com>
2025-07-16 21:17:35 -0300

    feat: add hooks and popcorn combo banner

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
     <Header />
     <Banner src={"./Imagens/Banner Desktop.png"} alt={"Banner do Luz e Cena para Desktop, com imagem de pessoas no cinema e formas abstratas"} />
     <MovieSection />
+    <Banner src={"./Imagens/Banner_Combo_Desktop.png"} alt={"Banner do Combo de Pipoca Estelar"} />
   </>
   )
 }
